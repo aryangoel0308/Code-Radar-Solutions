ee4049479c65aed9d230561caafc6b3fad228e48
Aryan Goel <aryan0269.becse24@chitkara.edu.in>
2025-02-10 10:37:02 +0530

    Solution to Check Less Than

---
+++ Check Less Than@@67882a731b4392d8fc1577d7/code.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int a, b;
+    if (a < b){
+        printf("True");
+    }
+    else if (a>b){
+        printf("False");
+    }
+    else if(a=b){
+        printf("False")
+    }
+    return 0;
+}
