0afab0dae239cddfd65201bfe6ba35ee5df35cf2
Aryan Goel <aryan0269.becse24@chitkara.edu.in>
2025-02-13 15:07:50 +0530

    Solution to Grade Evaluation

--- Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -2,19 +2,19 @@
 int main() {
     char ch;
     scanf("%c", &ch);
-    if(a == A){
+    if(ch == A){
         printf("Excellent");
     }
-    else if(a == 'B'){
+    else if(ch == 'B'){
         printf("Good");
     }
-    else if(a == 'C'){
+    else if(ch == 'C'){
         printf("Average");
     }
-    else if(a == 'D'){
+    else if(ch == 'D'){
         printf("Below Average");
     }
-    else if(a == 'F'){
+    else if(ch == 'F'){
         printf("Fail");
     }
     return 0;
