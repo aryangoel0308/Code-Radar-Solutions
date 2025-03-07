163082766b783b83e8c5c5b2fb8751e9ee0b111f
Aryan Goel <aryan0269.becse24@chitkara.edu.in>
2025-03-07 14:23:19 +0530

    Solution to Logical AND and OR Combination

--- Logical AND and OR Combination@@67882a731b4392d8fc1577df/code.c
+++ Logical AND and OR Combination@@67882a731b4392d8fc1577df/code.c
@@ -10,12 +10,7 @@ void checkNumbers(int a, int b) {
 
 int main() {
     int num1, num2;
-    
-    // Taking input from user
-    printf("Enter first number: ");
     scanf("%d", &num1);
-    
-    printf("Enter second number: ");
     scanf("%d", &num2);
     
     // Function call to check the condition
